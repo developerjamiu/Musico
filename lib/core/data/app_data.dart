0057756663ba00cadb0e9dd974628ae48c61d21d
Okanlawon Jamiu <developerjamiu@gmail.com>
2020-09-03 09:12:53 +0100

    Added App Data

--- lib/core/data/app_data.dart
+++ lib/core/data/app_data.dart
@@ -1,7 +1,118 @@
 import 'package:musico/model/music.dart';
 
+Music currentMusic = Music(
+  artist: 'Wizkid',
+  name: 'Ayo (Joy)',
+  image: 'assets/wizkid.jpg',
+);
+
 List<Music> recentlyAdded = <Music>[
   Music(
     artist: 'Adele',
+    name: 'Someone Like You',
+    image: 'assets/adele.jpg',
+  ),
+  Music(
+    artist: 'Olamide',
+    name: 'Motigbana',
+    image: 'assets/olamide.jpg',
+  ),
+  Music(
+    artist: 'Ayomikun',
+    name: 'Lambebe',
+    image: 'assets/cardic.jpg',
+  ),
+];
+
+List<Music> allSongs = <Music>[
+  Music(
+    artist: 'Olamide',
+    name: 'Motigbana',
+    image: 'assets/olamide.jpg',
+  ),
+  Music(
+    artist: 'Burna Boy',
+    name: 'Odogwu',
+    image: 'assets/burna1.jpg',
+  ),
+  Music(
+    artist: 'Davido',
+    name: 'Gobe',
+    image: 'assets/davido1.jpg',
+  ),
+  Music(
+    artist: 'Burna Boy',
+    name: '23',
+    image: 'assets/burna1.jpg',
+  ),
+  Music(
+    artist: 'Wizkid',
+    name: 'Ayo (Joy)',
+    image: 'assets/wizkid.jpg',
+  ),
+  Music(
+    artist: 'Teni',
+    name: 'Case',
+    image: 'assets/wizkid.jpg',
+  ),
+];
+
+List<Music> albums = <Music>[
+  Music(
+    artist: 'Burna Boy',
+    name: 'Twice As Tall',
+    image: 'assets/burna2.jpg',
+  ),
+  Music(
+    artist: 'Fireboy',
+    name: 'Apolio',
+    image: 'assets/fireboy1.jpg',
+  ),
+  Music(
+    artist: 'Fireboy',
+    name: 'Laughter, Tears and Goosebumps',
+    image: 'assets/fireboy2.jpg',
+  ),
+  Music(
+    artist: 'Tiwa Savage',
+    name: '49-99',
+    image: 'assets/tiwa1.jpg',
+  ),
+  Music(
+    artist: 'Cardi B',
+    name: 'Privacy',
+    image: 'assets/cardib1.jpg',
+  ),
+  Music(
+    artist: 'J Cole',
+    name: 'The Come Up',
+    image: 'assets/jcole.jpeg',
+  ),
+];
+
+List<Music> artists = <Music>[
+  Music(
+    artist: 'Burna Boy',
+    image: 'assets/burna2.jpg',
+  ),
+  Music(
+    artist: 'Davido',
+    image: 'assets/davido1.jpg',
+  ),
+  Music(
+    artist: 'Fireboy',
+    image: 'assets/fireboy2.jpg',
+  ),
+  Music(
+    artist: 'Rema',
+    image: 'assets/rema1.jpeg',
+  ),
+  Music(
+    artist: 'Tiwa Savage',
+    image: 'assets/tiwa1.jpg',
+  ),
+  Music(
+    artist: 'Cardi B',
+    image: 'assets/cardib2.jpg',
   ),
 ];
